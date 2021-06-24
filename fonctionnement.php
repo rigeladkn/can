80836a20a49471fe2e467bf007490cc6f9c9a8f6
rigel <rigeladikan@gmail.com>
2021-06-24 09:47:19 +0100

    version actuelle

--- fonctionnement.php
+++ fonctionnement.php
@@ -41,7 +41,8 @@
 	        	'<li class="nav-item"><a href="actualites.php" id="studies" class="nav-link">Actualités</a></li>'+
 	        	'<li class="nav-item" ><a href="services.php" onclick="" class="nav-link">Services</a></li>'+
 	          '<li class="nav-item"><a href="contact.php" id="contact" class="nav-link">Contact</a></li>'+
-	        '</ul>';
+            '</ul>';
+            
 	  }
     </script>
 
